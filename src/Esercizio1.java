d209d6ab39e0ed3738ea33662566d0e8d9d44215
lauraoliveri <lauraoliveri2003@gmail.com>
2025-06-04 13:18:52 +0200

    Esercizio 1 completato

---
+++ src/Esercizio1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Esercizio1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Inserisci un primo numero");
+
+        int numerouno = scan.nextInt();
+
+        System.out.println("Inserisci un primo numero");
+
+        int numerodue = scan.nextInt();
+
+        System.out.println("Inserisci un primo numero");
+
+        int numerotre = scan.nextInt();
+
+        if (numerouno > numerodue && numerouno> numerotre) {
+            System.out.println("Il primo numero è il maggiore");
+        }else {
+            System.out.println("Il primo numero NON è il maggiore");
+        }
+    }
+}
